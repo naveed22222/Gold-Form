e04d0ee6435ced761b5c3e6d6696d7dcd9e102d5
naveed22222 <naveedfarooq692@gmail.com>
2024-01-18 22:36:50 +0500

    Initial commit

---
+++ MyJquery.js
@@ -0,0 +1,16 @@
+
+	$(document).ready(function(){
+		$("input").focus(function(){
+			$(this).css("background-color","#f7eb72")
+		
+	});
+	$("input").blur(function(){
+			$(this).css("background-color","white")
+		
+	});
+	   
+	
+	});
+		
+		   
+		
